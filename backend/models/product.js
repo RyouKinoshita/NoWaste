c9b37c097e2dc5782884417ca888b46f81fe82bc
RyouKinoshita <ryoukinoshita@gmail.com>
2024-02-11 22:56:05 +0800

    product backend test

---
+++ backend/models/product.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter product name"],
+    trim: true,
+    maxLength: [100, "Product name cannot exceed 100 characters"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter product price"],
+    maxLength: [5, "Product name cannot exceed 5 characters"],
+    default: 0.0,
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter product description"],
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: [true, "Please select category for this product"],
+    enum: {
+      values: ["Vegetable", "Grains", "Fruits", "Nuts", "Root Crops"],
+      message: "Please select correct category for product",
+    },
+  },
+  assignedTo: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+  },
+  seller: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  stock: {
+    type: Number,
+    required: [true, "Please enter product stock"],
+    maxLength: [5, "Product name cannot exceed 5 characters"],
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
---
+++ backend/routes/product.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const upload = require("../utils/multer");
+
+const {
+  newProduct,
+  getProducts,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+  getAdminProducts,
+} = require("../controllers/productController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
+
+// router.get("/products", getProducts);
+
+// router.get("/product/:id", getSingleProduct);
+// router.get(
+//   "/admin/products",
+//   isAuthenticatedUser,
+//   authorizeRoles("admin", "seller"),
+//   getAdminProducts
+// );
+
+// router
+//   .route(
+//     "/admin/product/:id",
+//     isAuthenticatedUser,
+//     authorizeRoles("admin", "seller")
+//   )
+//   .put(upload.array("images", 10), updateProduct)
+//   .delete(deleteProduct);
+
+router.post(
+  "/admin/product/new",
+  isAuthenticatedUser,
+  authorizeRoles("admin", "seller"),
+  upload.array("images", 10),
+  newProduct
+);
+
+module.exports = router;
