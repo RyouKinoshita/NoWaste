e5f4a4e7619a8b99f50ecb003517e965b5fca4ef
Dottyyyyy <Orekihoutarou126@gmail.com>
2024-02-14 20:16:12 +0800

    Modify Header

--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -13,6 +13,16 @@ import {
   MDBModalBody,
   MDBModalFooter,
 } from 'mdb-react-ui-kit';
+
+import {
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem
+
+} from 'mdb-react-ui-kit';
+
+
 const Header = () => {
   const [fullscreenXlModal, setFullscreenXlModal] = useState(false);
 
@@ -67,23 +77,94 @@ const Header = () => {
                   Contact Us
                 </a>
               </li>
-            </ul>
-            <ul className="nav navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link text-white" href="https://twitter.com/CreativeTim">
-                  <i className="fab fa-twitter" />
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-white mx-2" href="https://www.facebook.com/CreativeTim">
-                  <i className="fab fa-facebook" />
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-white" href="https://www.instagram.com/CreativeTimOfficial">
-                  <i className="fab fa-instagram" />
-                </a>
-              </li>
+
+              <div className="nav-item ml-auto">
+                <MDBDropdown>
+                  <MDBDropdownToggle outline rounded>
+                    Others
+                  </MDBDropdownToggle>
+                  <MDBDropdownMenu>
+                    <MDBDropdownItem>
+                      <a className="nav-link text-black" onClick={toggleFullScreen}>
+                        Option 1
+                      </a>
+                      <MDBModal tabIndex='-1' open={fullscreenXlModal} setOpen={setFullscreenXlModal}>
+                        <MDBModalDialog size='fullscreen'>
+                          <MDBModalContent>
+                            <MDBModalHeader>
+                              <MDBModalTitle>TUP-TAGUIG</MDBModalTitle>
+                              <MDBBtn
+                                type='button'
+                                className='btn-close'
+                                color='none'
+                                onClick={toggleFullScreen}
+                              ></MDBBtn>
+                            </MDBModalHeader>
+                            <MDBModalBody>...</MDBModalBody>
+                            <MDBModalFooter>
+                              <MDBBtn type='button' color='secondary' onClick={toggleFullScreen}>
+                                Close
+                              </MDBBtn>
+                            </MDBModalFooter>
+                          </MDBModalContent>
+                        </MDBModalDialog>
+                      </MDBModal>
+                    </MDBDropdownItem>
+                    <MDBDropdownItem>
+                      <a className="nav-link text-black" onClick={toggleFullScreen}>
+                        Option 2
+                      </a>
+                      <MDBModal tabIndex='-1' open={fullscreenXlModal} setOpen={setFullscreenXlModal}>
+                        <MDBModalDialog size='fullscreen'>
+                          <MDBModalContent>
+                            <MDBModalHeader>
+                              <MDBModalTitle>TUP-TAGUIG</MDBModalTitle>
+                              <MDBBtn
+                                type='button'
+                                className='btn-close'
+                                color='none'
+                                onClick={toggleFullScreen}
+                              ></MDBBtn>
+                            </MDBModalHeader>
+                            <MDBModalBody>...</MDBModalBody>
+                            <MDBModalFooter>
+                              <MDBBtn type='button' color='secondary' onClick={toggleFullScreen}>
+                                Close
+                              </MDBBtn>
+                            </MDBModalFooter>
+                          </MDBModalContent>
+                        </MDBModalDialog>
+                      </MDBModal>
+                    </MDBDropdownItem>
+                    <MDBDropdownItem>
+                      <a className="nav-link text-black" onClick={toggleFullScreen}>
+                        Option 3
+                      </a>
+                      <MDBModal tabIndex='-1' open={fullscreenXlModal} setOpen={setFullscreenXlModal}>
+                        <MDBModalDialog size='fullscreen'>
+                          <MDBModalContent>
+                            <MDBModalHeader>
+                              <MDBModalTitle>TUP-TAGUIG</MDBModalTitle>
+                              <MDBBtn
+                                type='button'
+                                className='btn-close'
+                                color='none'
+                                onClick={toggleFullScreen}
+                              ></MDBBtn>
+                            </MDBModalHeader>
+                            <MDBModalBody>...</MDBModalBody>
+                            <MDBModalFooter>
+                              <MDBBtn type='button' color='secondary' onClick={toggleFullScreen}>
+                                Close
+                              </MDBBtn>
+                            </MDBModalFooter>
+                          </MDBModalContent>
+                        </MDBModalDialog>
+                      </MDBModal>
+                    </MDBDropdownItem>
+                  </MDBDropdownMenu>
+                </MDBDropdown>
+              </div>
             </ul>
           </div>
         </div>
