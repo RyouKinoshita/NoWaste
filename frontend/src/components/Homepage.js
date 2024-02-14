042761b4830e248bf53fcc44145f1461a7eacbc5
itsk2 <codonkellyn3@gmail.com>
2024-02-15 01:36:52 +0800

    Login Modified

---
+++ frontend/src/components/Homepage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from "./layouts/Header";
+import Body from './layouts/Body';
+import Footer from './layouts/Footer';
+
+
+
+const Homepage = () => {
+    return (
+        <div>
+            <>
+            
+                <Header />
+                <Body />
+                <Footer/>
+              
+            </>
+        </div>
+    )
+}
+
+export default Homepage
