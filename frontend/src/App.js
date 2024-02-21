26da531a3b307ab1d597f8e30e4e932f44fefaf0
Dottyyyyy <Orekihoutarou126@gmail.com>
2024-02-22 00:06:22 +0800

    Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 import Homepage from "./components/Homepage";
+import Login from "./components/user/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Homepage />
+      <Router >
+        <Routes>
+          <Route path="/" element={< Login />} caseSensitive={true}/>
+          <Route path="/homepage" element={< Homepage />} exact="true"/>
+        </Routes>
+      </Router>
     </>
   );
 }
