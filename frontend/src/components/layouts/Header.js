d02afdac3a4152a16359bc4516666bd80b94fb93
RyouKinoshita <ryoukinoshita@gmail.com>
2024-02-23 15:50:48 +0800

    contact us fix

--- frontend/src/components/layouts/Header.js
+++ frontend/src/components/layouts/Header.js
@@ -15,36 +15,42 @@ import {
   MDBDropdownMenu,
   MDBNavbar,
   MDBRipple,
-  MDBIcon
+  MDBIcon,
 } from "mdb-react-ui-kit";
 import "../../index.css";
-import { Link, useNavigate } from 'react-router-dom';
-
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
-
-
   const [fullscreenXlModal, setFullscreenXlModal] = useState(false);
   const toggleFullScreen = () => setFullscreenXlModal(!fullscreenXlModal);
   const [centredModal, setCentredModal] = useState(false);
   const toggleOpen = () => setCentredModal(!centredModal);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const logoutHandler = () => {
-    localStorage.clear()
-    alert('You are now Logout!')
-    navigate('/')
-  }
-  console.log(logoutHandler)
+    localStorage.clear();
+    alert("You are now Logout!");
+    navigate("/");
+  };
+  console.log(logoutHandler);
   return (
-
-
-    <header >
-
-
-      <nav className="navbar navbar-expand-lg navbar-dark navbar-absolute bg-transparent shadow-none" style={{ backgroundImage: 'url("../assets/images/waster.png")' }} >
+    <header>
+      <nav
+        className="navbar navbar-expand-lg navbar-dark navbar-absolute bg-transparent shadow-none"
+        style={{ backgroundImage: 'url("../assets/images/waster.png")' }}
+      >
         <div className="container">
-          <a className="navbar-brand text-white" href="javascript:;">No waste System</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-header-2" aria-controls="navbar-header-2" aria-expanded="false" aria-label="Toggle navigation">
+          <a className="navbar-brand text-white" href="javascript:;">
+            No waste System
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbar-header-2"
+            aria-controls="navbar-header-2"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbar-header-2">
@@ -66,20 +72,18 @@ const Header = () => {
               </li> */}
             </ul>
             <ul className="nav navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link text-white" >
+              <li className="nav-item nav-link text-white">
+                <Link to="/homepage" style={{ color: "white" }}>
                   Home
-                </a>
+                </Link>
               </li>
               <li className="nav-item nav-link text-white">
-                <Link to='/aboutus' style={{ color: "white" }}>
+                <Link to="/aboutus" style={{ color: "white" }}>
                   About Us
                 </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link text-white" >
-                  Contact Us
-                </a>
+                <a className="nav-link text-white">Contact Us</a>
               </li>
               {/* <li className="nav-item nav-link text-white"  >
                 <Link to='/login' style={{ color: "white" }}>
@@ -87,44 +91,59 @@ const Header = () => {
                 </Link>
               </li> */}
 
-              <li className="nav-item nav-link text-white" >
-                <a onClick={logoutHandler} ><i class="fa-solid fa-user-xmark" style={{cursor:'pointer'}}></i></a>
+              <li className="nav-item nav-link text-white">
+                <a onClick={logoutHandler}>
+                  <i
+                    class="fa-solid fa-user-xmark"
+                    style={{ cursor: "pointer" }}
+                  ></i>
+                </a>
               </li>
             </ul>
           </div>
         </div>
       </nav>
 
-
-      <div className="page-header min-vh-100" style={{
-        backgroundImage: 'url("../assets/images/waster.png")', backgroundSize: 'cover', // You can adjust this property
-        backgroundRepeat: 'no-repeat',
-        width: '100vw',
-        height: '100vh',
-      }}
-        loading="lazy" >
-
-
+      <div
+        className="page-header min-vh-100"
+        style={{
+          backgroundImage: 'url("../assets/images/waster.png")',
+          backgroundSize: "cover", // You can adjust this property
+          backgroundRepeat: "no-repeat",
+          width: "100vw",
+          height: "100vh",
+        }}
+        loading="lazy"
+      >
         <div className="container">
           <div className="row">
             <div className="col-lg-6 col-md-7 d-flex justify-content-center flex-column my-5">
-              <h1 className="text-white mb-6" style={{ fontSize: '4rem' }}>Bridging waste from Taguig Palengke to Pig Farms</h1>
-              <p className="text-white opacity-8 lead pe-5 me-5">Strengthening and sustaining the Philippines' agricultural ecology and creating opportunities that help align with the government’s efforts in combating the problems with waste management.</p>
+              <h1 className="text-white mb-6" style={{ fontSize: "4rem" }}>
+                Bridging waste from Taguig Palengke to Pig Farms
+              </h1>
+              <p className="text-white opacity-8 lead pe-5 me-5">
+                Strengthening and sustaining the Philippines' agricultural
+                ecology and creating opportunities that help align with the
+                government’s efforts in combating the problems with waste
+                management.
+              </p>
               <div className="buttons">
-                <button type="button" className="btn btn-white mt-4">Get Started</button>
-                <button type="button" className="btn text-white shadow-none mt-4">Read more</button>
+                <button type="button" className="btn btn-white mt-4">
+                  Get Started
+                </button>
+                <button
+                  type="button"
+                  className="btn text-white shadow-none mt-4"
+                >
+                  Read more
+                </button>
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </header>
-
   );
 };
 
 export default Header;
-
-
-
--- frontend/src/components/user/Login.js
+++ frontend/src/components/user/Login.js
@@ -1,113 +1,154 @@
 import { useNavigate } from "react-router-dom";
-import { authenticate } from "../utils/helpers"
-import axios from 'axios'
+import { authenticate } from "../utils/helpers";
+import axios from "axios";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-    MDBInput,
-    MDBCol,
-    MDBRow,
-    MDBCheckbox,
-    MDBBtn
-} from 'mdb-react-ui-kit';
+  MDBInput,
+  MDBCol,
+  MDBRow,
+  MDBCheckbox,
+  MDBBtn,
+} from "mdb-react-ui-kit";
 
 export default function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
-    const login = async (email, password) => {
-        try {
-            const config = {
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            };
-            // const { data } = await axios.post(${process.env.REACT_APP_API}/api/v1/login, form, config)
-            const { data } = await axios.post(
-                `http://localhost:4001/api/v1/login`,
-                { email, password },
-                config
-            );
-            console.log(data);
-            localStorage.setItem('user', JSON.stringify(data));
-            authenticate(data, () => navigate("/homepage"));
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const login = async (email, password) => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      // const { data } = await axios.post(${process.env.REACT_APP_API}/api/v1/login, form, config)
+      const { data } = await axios.post(
+        `http://localhost:4001/api/v1/login`,
+        { email, password },
+        config
+      );
+      console.log(data);
+      localStorage.setItem("user", JSON.stringify(data));
+      authenticate(data, () => navigate("/homepage"));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+  };
 
-        } catch (error) {
-            console.log(error);
-        }
-    };
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        login(email, password);
-    };
+  return (
+    // <div className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
+    //     <form onSubmit={handleSubmit} style={{ width: "300px" }}>
+    //         <MDBRow className='mb-4'>
+    //             <MDBCol>
+    //                 <MDBInput className='mb-4' type='email' label='Email address' value={email}
+    //                     onChange={(e) => setEmail(e.target.value)} />
+    //                 <MDBInput className='mb-4' type='password' label='Password' value={password}
+    //                     onChange={(e) => setPassword(e.target.value)} />
+    //             </MDBCol>
+    //         </MDBRow>
 
-    return (
-        // <div className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
-        //     <form onSubmit={handleSubmit} style={{ width: "300px" }}>
-        //         <MDBRow className='mb-4'>
-        //             <MDBCol>
-        //                 <MDBInput className='mb-4' type='email' label='Email address' value={email}
-        //                     onChange={(e) => setEmail(e.target.value)} />
-        //                 <MDBInput className='mb-4' type='password' label='Password' value={password}
-        //                     onChange={(e) => setPassword(e.target.value)} />
-        //             </MDBCol>
-        //         </MDBRow>
+    //         <MDBRow className='mb-4'>
+    //             <MDBCol size='auto'>
+    //                 <MDBCheckbox id='form1Example3' label='Remember me' defaultChecked />
+    //             </MDBCol>
+    //             <MDBCol size='auto'>
+    //                 <a href='#!'>Forgot password?</a>
+    //             </MDBCol>
+    //         </MDBRow>
 
-        //         <MDBRow className='mb-4'>
-        //             <MDBCol size='auto'>
-        //                 <MDBCheckbox id='form1Example3' label='Remember me' defaultChecked />
-        //             </MDBCol>
-        //             <MDBCol size='auto'>
-        //                 <a href='#!'>Forgot password?</a>
-        //             </MDBCol>
-        //         </MDBRow>
+    //         <MDBBtn type='submit' size="sm" block>
+    //             Sign in
+    //         </MDBBtn>
+    //     </form>
+    // </div>
 
-        //         <MDBBtn type='submit' size="sm" block>
-        //             Sign in
-        //         </MDBBtn>
-        //     </form>
-        // </div>
-
-
-        <section className="vh-100" style={{ backgroundImage: 'url("../assets/images/wall.jpg")' }} >
-            <div className="container py-5 h-100">
-                <div className="row d-flex justify-content-center align-items-center h-100">
-                    <div className="col col-xl-10" style={{ color: '#76FF03' }} >
-                        <div className="card" style={{ borderRadius: '1rem' }}>
-                            <div className="row g-0">
-                                <div className="col-md-6 col-lg-5 d-none d-md-block">
-                                    <img src="../assets/images/feed-more.jpg" alt="login form" className="img-fluid" style={{ borderRadius: '1rem 0 0 1rem' }} />
-                                </div>
-                                <div className="col-md-6 col-lg-7 d-flex align-items-center">
-                                    <div className="card-body p-4 p-lg-5 text-black">
-                                        <form onSubmit={handleSubmit}>
-                                            <div className="d-flex align-items-center mb-3 pb-1">
-                                                <i className="fas fa-cubes fa-2x me-3" style={{ color: '#ff6219' }} />
-                                                <span className="h2 fw-bold mb-0">No Waste System</span>
-                                            </div>
-                                            <h5 className="fw-normal mb-3 pb-3" style={{ letterSpacing: 1 }}>Sign into your account</h5>
-                                            <div className="form-outline mb-4">
-                                                <MDBInput className='mb-4' type='email' label='Email address' value={email} onChange={(e) => setEmail(e.target.value)} />
-                                            </div>
-                                            <div className="form-outline mb-4">
-                                                <MDBInput className='mb-4' type='password' label='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
-                                            </div>
-                                            <div className="pt-1 mb-4">
-                                                <button className="btn btn-dark btn-lg btn-block" type="submit">Login</button>
-                                            </div>
-                                            <a className="small text-muted" href="#!">Forgot password?</a>
-                                            <p className="mb-5 pb-lg-2" style={{ color: '#393f81' }}>Don't have an account? <a href="#!" style={{ color: '#393f81' }}>Register here</a></p>
-                                            <a href="#!" className="small text-muted">Terms of use.</a>
-                                            <a href="#!" className="small text-muted">Privacy policy</a>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+    <section
+      className="vh-100"
+      style={{ backgroundImage: 'url("../assets/images/wall.jpg")' }}
+    >
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col col-xl-10" style={{ color: "#76FF03" }}>
+            <div className="card" style={{ borderRadius: "1rem" }}>
+              <div className="row g-0">
+                <div className="col-md-6 col-lg-5 d-none d-md-block">
+                  <img
+                    src="../assets/images/feed-more.jpg"
+                    alt="login form"
+                    className="img-fluid"
+                    style={{ borderRadius: "1rem 0 0 1rem" }}
+                  />
                 </div>
+                <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                  <div className="card-body p-4 p-lg-5 text-black">
+                    <form onSubmit={handleSubmit}>
+                      <div className="d-flex align-items-center mb-3 pb-1">
+                        <i
+                          className="fas fa-cubes fa-2x me-3"
+                          style={{ color: "#ff6219" }}
+                        />
+                        <span className="h2 fw-bold mb-0">No Waste System</span>
+                      </div>
+                      <h5
+                        className="fw-normal mb-3 pb-3"
+                        style={{ letterSpacing: 1 }}
+                      >
+                        Sign into your account
+                      </h5>
+                      <div className="form-outline mb-4">
+                        <MDBInput
+                          className="mb-4"
+                          type="email"
+                          label="Email address"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                        />
+                      </div>
+                      <div className="form-outline mb-4">
+                        <MDBInput
+                          className="mb-4"
+                          type="password"
+                          label="Password"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                        />
+                      </div>
+                      <div className="pt-1 mb-4">
+                        <button
+                          className="btn btn-dark btn-lg btn-block"
+                          type="submit"
+                        >
+                          Login
+                        </button>
+                      </div>
+                      <a className="small text-muted" href="#!">
+                        Forgot password?
+                      </a>
+                      <p className="mb-5 pb-lg-2" style={{ color: "#393f81" }}>
+                        Don't have an account?{" "}
+                        <a href="#!" style={{ color: "#393f81" }}>
+                          Register here
+                        </a>
+                      </p>
+                      <a href="#!" className="small text-muted">
+                        Terms of use.
+                      </a>
+                      <a href="#!" className="small text-muted">
+                        Privacy policy
+                      </a>
+                    </form>
+                  </div>
+                </div>
+              </div>
             </div>
-        </section>
-
-    );
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
