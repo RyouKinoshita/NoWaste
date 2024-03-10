fab5ea7571f382126fb1f626294fb8f2744922a9
itsk2 <codonkellyn3@gmail.com>
2024-03-11 01:28:58 +0800

    AppBackend

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
@@ -6,6 +7,7 @@ const auth = require("./routes/auth");
 const product = require("./routes/product");
 const order = require("./routes/order");
 const article = require("./routes/article");
+const cart = require("./routes/cart");
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
@@ -17,5 +19,6 @@ app.use("/api/v1", auth);
 app.use("/api/v1", product);
 app.use("/api/v1", order);
 app.use("/api/v1", article);
+app.use("/api/v1", cart);
 
 module.exports = app;
